16c87607fde5d1dcacda8dbca31a4e5ac57c09a8
Ben New <ben.new@gaiaresources.com.au>
2016-05-31 03:17:02 +0800

    Add missing dependency.

--- public/app/ui/pages/search/photographs/resultFields.js
+++ public/app/ui/pages/search/photographs/resultFields.js
@@ -1,60 +1,65 @@
 (function () {
     'use strict';
 
-    define(function () {
-        return [
-            {
-                key: 'Title',
-                labelText: 'Title'
-            },
-            {
-                key: 'Creator',
-                labelText: 'Creator',
-                displayValue: function (value) {
-                    return '<div>' + _(value.split(';'))
-                            .chunk(2)
-                            .map(function (tuple) {
-                                return tuple[0] + ': ' + tuple[1];
-                            })
-                            .uniq()
-                            .join('</div><div>') + '</div>';
+    define(
+        [
+            'lodash'
+        ],
+        function (_) {
+            return [
+                {
+                    key: 'Title',
+                    labelText: 'Title'
+                },
+                {
+                    key: 'Creator',
+                    labelText: 'Creator',
+                    displayValue: function (value) {
+                        return '<div>' + _(value.split(';'))
+                                .chunk(2)
+                                .map(function (tuple) {
+                                    return tuple[0] + ': ' + tuple[1];
+                                })
+                                .uniq()
+                                .join('</div><div>') + '</div>';
+                    }
+                },
+                {
+                    key: 'DateOfCreation',
+                    labelText: 'Date of Creation',
+                    displayValue: function (value) {
+                        var date = new Date(Date.parse(value));
+                        return '<span title="' + date.toDateString() + '" data-microtime="' + date.getTime() + '">' + date.toLocaleDateString() + '</span>';
+                    }
+                },
+                {
+                    key: 'Publisher',
+                    labelText: 'Publisher'
+                },
+                {
+                    key: 'DateOfPublication',
+                    labelText: 'Date of Publication',
+                    displayValue: function (value) {
+                        var date = new Date(Date.parse(value));
+                        return '<span title="' + date.toDateString() + '" data-microtime="' + date.getTime() + '">' + date.toLocaleDateString() + '</span>';
+                    }
+                },
+                {
+                    key: 'PlaceOfPublication',
+                    labelText: 'Place of Publication'
+                },
+                {
+                    key: 'Medium',
+                    labelText: 'Medium'
+                },
+                {
+                    key: 'Subjects',
+                    labelText: 'Subjects',
+                    displayValue: function (value) {
+                        return '<ul class="list-unstyled"><li>' + value.replace(';', '</li><li>') + '</li></ul>';
+                    }
                 }
-            },
-            {
-                key: 'DateOfCreation',
-                labelText: 'Date of Creation',
-                displayValue: function (value) {
-                    var date = new Date(Date.parse(value));
-                    return '<span title="' + date.toDateString() + '" data-microtime="' + date.getTime() + '">' + date.toLocaleDateString() + '</span>';
-                }
-            },
-            {
-                key: 'Publisher',
-                labelText: 'Publisher'
-            },
-            {
-                key: 'DateOfPublication',
-                labelText: 'Date of Publication',
-                displayValue: function (value) {
-                    var date = new Date(Date.parse(value));
-                    return '<span title="' + date.toDateString() + '" data-microtime="' + date.getTime() + '">' + date.toLocaleDateString() + '</span>';
-                }
-            },
-            {
-                key: 'PlaceOfPublication',
-                labelText: 'Place of Publication'
-            },
-            {
-                key: 'Medium',
-                labelText: 'Medium'
-            },
-            {
-                key: 'Subjects',
-                labelText: 'Subjects',
-                displayValue: function (value) {
-                    return '<ul class="list-unstyled"><li>' + value.replace(';', '</li><li>') + '</li></ul>';
-                }
-            }
-        ];
-    });
+            ];
+        }
+    );
 }());
