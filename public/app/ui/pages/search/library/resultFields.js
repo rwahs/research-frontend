55b373d056b989df5126ea0d15ed7071ceb287cf
Ben New <ben.new@gaiaresources.com.au>
2016-05-31 03:17:33 +0800

    Explicitly declare no dependencies.

--- public/app/ui/pages/search/library/resultFields.js
+++ public/app/ui/pages/search/library/resultFields.js
@@ -1,46 +1,49 @@
 (function () {
     'use strict';
 
-    define(function () {
-        return [
-            {
-                key: 'Title',
-                labelText: 'Title'
-            },
-            {
-                key: 'Author',
-                labelText: 'Author',
-                displayValue: function (value) {
-                    return '<ul class="list-unstyled"><li>' + value.replace(';', '</li><li>') + '</li></ul>';
+    define(
+        [],
+        function () {
+            return [
+                {
+                    key: 'Title',
+                    labelText: 'Title'
+                },
+                {
+                    key: 'Author',
+                    labelText: 'Author',
+                    displayValue: function (value) {
+                        return '<ul class="list-unstyled"><li>' + value.replace(';', '</li><li>') + '</li></ul>';
+                    }
+                },
+                {
+                    key: 'Publisher',
+                    labelText: 'Publisher'
+                },
+                {
+                    key: 'DateOfPublication',
+                    labelText: 'Date of Publication',
+                    displayValue: function (value) {
+                        var date = new Date(Date.parse(value));
+                        return '<span title="' + date.toDateString() + '" data-microtime="' + date.getTime() + '">' + date.toLocaleDateString() + '</span>';
+                    }
+                },
+                {
+                    key: 'PlaceOfPublication',
+                    labelText: 'Place of Publication'
+                },
+                {
+                    key: 'PublicationType',
+                    labelText: 'Type'
+                },
+                {
+                    key: 'Subjects',
+                    labelText: 'Subjects',
+                    displayValue: function (value) {
+                        return '<ul class="list-unstyled"><li>' + value.replace(';', '</li><li>') + '</li></ul>';
+                    }
                 }
-            },
-            {
-                key: 'Publisher',
-                labelText: 'Publisher'
-            },
-            {
-                key: 'DateOfPublication',
-                labelText: 'Date of Publication',
-                displayValue: function (value) {
-                    var date = new Date(Date.parse(value));
-                    return '<span title="' + date.toDateString() + '" data-microtime="' + date.getTime() + '">' + date.toLocaleDateString() + '</span>';
-                }
-            },
-            {
-                key: 'PlaceOfPublication',
-                labelText: 'Place of Publication'
-            },
-            {
-                key: 'PublicationType',
-                labelText: 'Type'
-            },
-            {
-                key: 'Subjects',
-                labelText: 'Subjects',
-                displayValue: function (value) {
-                    return '<ul class="list-unstyled"><li>' + value.replace(';', '</li><li>') + '</li></ul>';
-                }
-            }
-        ];
-    });
+            ];
+        }
+    );
 }());
--- public/app/ui/pages/search/museum/resultFields.js
+++ public/app/ui/pages/search/museum/resultFields.js
@@ -1,39 +1,42 @@
 (function () {
     'use strict';
 
-    define(function () {
-        return [
-            {
-                key: 'type',
-                labelText: 'Item Type'
-            },
-            {
-                key: 'idno',
-                labelText: 'Accession Number'
-            },
-            {
-                key: 'ItemName',
-                labelText: 'Item Name'
-            },
-            {
-                key: 'Dates',
-                labelText: 'Dates'
-            },
-            {
-                key: 'Importance',
-                labelText: 'Importance'
-            },
-            {
-                key: 'Subjects',
-                labelText: 'Subjects',
-                displayValue: function (value) {
-                    return '<ul class="list-unstyled"><li>' + value.replace(';', '</li><li>') + '</li></ul>';
+    define(
+        [],
+        function () {
+            return [
+                {
+                    key: 'type',
+                    labelText: 'Item Type'
+                },
+                {
+                    key: 'idno',
+                    labelText: 'Accession Number'
+                },
+                {
+                    key: 'ItemName',
+                    labelText: 'Item Name'
+                },
+                {
+                    key: 'Dates',
+                    labelText: 'Dates'
+                },
+                {
+                    key: 'Importance',
+                    labelText: 'Importance'
+                },
+                {
+                    key: 'Subjects',
+                    labelText: 'Subjects',
+                    displayValue: function (value) {
+                        return '<ul class="list-unstyled"><li>' + value.replace(';', '</li><li>') + '</li></ul>';
+                    }
+                },
+                {
+                    key: 'Classification',
+                    labelText: 'Classification'
                 }
-            },
-            {
-                key: 'Classification',
-                labelText: 'Classification'
-            }
-        ];
-    });
+            ];
+        }
+    );
 }());
