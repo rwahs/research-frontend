0b6df1afc7fda78e2b18f5a404efa50e498d634a
Ben New <ben.new@gaiaresources.com.au>
2016-05-31 01:12:07 +0800

    Add tests for `SearchPage` module.

---
+++ test/fixtures/search/searchTypes.js
@@ -0,0 +1,18 @@
+(function () {
+    'use strict';
+
+    define(function () {
+        return [
+            {
+                key: 'first',
+                labelText: 'Field One',
+                glyphicon: 'one'
+            },
+            {
+                key: 'second',
+                labelText: 'Field Two',
+                glyphicon: 'two'
+            }
+        ];
+    });
+}());
--- test/spec/ui/pages/search/SearchPage.spec.js
+++ test/spec/ui/pages/search/SearchPage.spec.js
@@ -4,14 +4,214 @@
     define(
         [
             'chai',
-            'ui/pages/search/SearchPage'
+            'sinon',
+            'knockout',
+            'util/container',
+            'ui/pages/search/SearchPage',
+            'mock/services/MockSearchService'
         ],
-        function (chai, SearchPage) {
+        function (chai, sinon, ko, container, SearchPage, MockSearchService) {
             var expect = chai.expect;
 
             describe('The `SearchPage` module', function () {
                 it('Defines a constructor function', function () {
                     expect(SearchPage).to.be.a('function');
+                });
+                describe('When the search service is returning valid results', function () {
+                    var collectionSearchService;
+                    beforeEach(function () {
+                        // jshint camelcase: false
+                        collectionSearchService = new MockSearchService(undefined, [
+                            { object_id: 1, first: 'First 1', second: 'Second 1', third: 'Third 1' },
+                            { object_id: 2, first: 'First 2', second: 'Second 2', third: 'Third 2' },
+                            { object_id: 3, first: 'First 3', second: 'Second 3', third: 'Third 3' }
+                        ]);
+                        container.register('CollectionSearchService', collectionSearchService);
+                        container.seal();
+                    });
+                    describe('When constructed with correct parameters', function () {
+                        var context, page;
+                        beforeEach(function () {
+                            context = {};
+                            page = new SearchPage(context, {
+                                collectionName: 'Collection',
+                                searchServiceKey: 'CollectionSearchService',
+                                detailUrlTemplate: 'path/to/detail/:id',
+                                searchTypes: 'fixtures/search/searchTypes',
+                                resultFields: 'fixtures/search/resultFields'
+                            });
+                        });
+                        it('Returns an object', function () {
+                            expect(page).to.be.an('object');
+                        });
+                        it('Exposes the correct observables and computed observables', function () {
+                            expect(ko.isObservable(page.searchText)).to.equal(true);
+                            expect(ko.isObservable(page.searchTypes)).to.equal(true);
+                            expect(ko.isObservable(page.resultFields)).to.equal(true);
+                            expect(ko.isObservable(page.results)).to.equal(true);
+                            expect(ko.isObservable(page.loading)).to.equal(true);
+                            expect(ko.isObservable(page.displayResults)).to.equal(true);
+                            expect(ko.isPureComputed(page.heading)).to.equal(true);
+                            expect(ko.isPureComputed(page.placeholder)).to.equal(true);
+                            expect(ko.isPureComputed(page.hasResults)).to.equal(true);
+                        });
+                        it('Exposes life cycle methods', function () {
+                            expect(page.binding).to.be.a('function');
+                        });
+                        it('Exposes event handlers', function () {
+                            expect(page.submit).to.be.a('function');
+                        });
+                        it('Gives the correct default values', function () {
+                            expect(page.searchText()).to.equal('');
+                            expect(page.searchTypes()).to.have.length(0);
+                            expect(page.resultFields()).to.have.length(0);
+                            expect(page.results()).to.have.length(0);
+                        });
+                        it('Is not loading or displaying results', function () {
+                            expect(page.loading()).to.equal(false);
+                            expect(page.displayResults()).to.equal(false);
+                        });
+                        it('Gives the right default computed values', function () {
+                            expect(page.heading()).to.equal('Collection Search');
+                            expect(page.placeholder()).to.equal('Enter your search terms...');
+                            expect(page.hasResults()).to.equal(false);
+                        });
+                        describe('When bound to the view', function () {
+                            var container;
+                            beforeEach(function (done) {
+                                container = document.createElement('div');
+                                page.binding(container, done);
+                            });
+                            it('Sets the search types', function () {
+                                expect(page.searchTypes()).to.have.length(2); // see fixtures/search/searchTypes.js
+                            });
+                            it('Sets the active search type', function () {
+                                expect(page.searchTypes()[0].active()).to.equal(true);
+                                expect(page.searchTypes()[1].active()).to.equal(false);
+                            });
+                            it('Sets the placeholder text', function () {
+                                expect(page.placeholder()).to.equal('Search by Field One...');
+                            });
+                            it('Sets the result fields', function () {
+                                expect(page.resultFields()).to.have.length(3); // see fixtures/search/resultFields.js
+                            });
+                            it('Is not loading or displaying results', function () {
+                                expect(page.loading()).to.equal(false);
+                                expect(page.displayResults()).to.equal(false);
+                            });
+                            describe('When a different search type is made active', function () {
+                                beforeEach(function () {
+                                    page.searchTypes()[1].makeActive();
+                                });
+                                it('Sets the active search type', function () {
+                                    expect(page.searchTypes()[0].active()).to.equal(false);
+                                    expect(page.searchTypes()[1].active()).to.equal(true);
+                                });
+                                it('Sets the placeholder text', function () {
+                                    expect(page.placeholder()).to.equal('Search by Field Two...');
+                                });
+                                it('Is not loading or displaying results', function () {
+                                    expect(page.loading()).to.equal(false);
+                                    expect(page.displayResults()).to.equal(false);
+                                });
+                            });
+                            describe('With empty search text', function () {
+                                beforeEach(function () {
+                                    page.searchTypes()[0].makeActive();
+                                    page.searchText('');
+                                });
+                                describe('When the search form is submitted', function () {
+                                    beforeEach(function () {
+                                        page.submit();
+                                    });
+                                    it('Calls the specified search service with a wildcard', function () {
+                                        sinon.assert.calledOnce(collectionSearchService);
+                                        sinon.assert.calledWith(collectionSearchService, { first: '*' });
+                                    });
+                                    it('Is not loading but is displaying results', function () {
+                                        expect(page.loading()).to.equal(false);
+                                        expect(page.displayResults()).to.equal(true);
+                                    });
+                                    it('Displays the retrieved results', function () {
+                                        expect(page.results()).to.have.length(3); // 3 records in mock data
+                                    });
+                                });
+                            });
+                            describe('With non-empty search text', function () {
+                                beforeEach(function () {
+                                    page.searchTypes()[1].makeActive();
+                                    page.searchText('query');
+                                });
+                                describe('When the search form is submitted', function () {
+                                    beforeEach(function () {
+                                        page.submit();
+                                    });
+                                    it('Calls the specified search service with the given search text', function () {
+                                        sinon.assert.calledOnce(collectionSearchService);
+                                        sinon.assert.calledWith(collectionSearchService, { second: 'query' });
+                                    });
+                                    it('Is not loading but is displaying results', function () {
+                                        expect(page.loading()).to.equal(false);
+                                        expect(page.displayResults()).to.equal(true);
+                                    });
+                                    it('Displays the retrieved results', function () {
+                                        expect(page.results()).to.have.length(3); // 3 records in mock data
+                                    });
+                                });
+                            });
+                        });
+                    });
+                    afterEach(function () {
+                        container.reset();
+                    });
+                });
+                describe('When the search service is returning errors', function () {
+                    var collectionSearchService;
+                    beforeEach(function () {
+                        // jshint camelcase: false
+                        collectionSearchService = new MockSearchService(new Error('Search Error'));
+                        container.register('CollectionSearchService', collectionSearchService);
+                        container.seal();
+                    });
+                    describe('When constructed with correct parameters', function () {
+                        var context, page;
+                        beforeEach(function () {
+                            context = {};
+                            page = new SearchPage(context, {
+                                collectionName: 'Collection',
+                                searchServiceKey: 'CollectionSearchService',
+                                detailUrlTemplate: 'path/to/detail/:id',
+                                searchTypes: 'fixtures/search/searchTypes',
+                                resultFields: 'fixtures/search/resultFields'
+                            });
+                        });
+                        describe('When bound to the view', function () {
+                            var container;
+                            beforeEach(function (done) {
+                                container = document.createElement('div');
+                                page.binding(container, done);
+                            });
+                            describe('When the search form is submitted', function () {
+                                beforeEach(function () {
+                                    page.submit();
+                                });
+                                it('Calls the specified search service', function () {
+                                    sinon.assert.calledOnce(collectionSearchService);
+                                });
+                                it('Is not loading or displaying results', function () {
+                                    expect(page.loading()).to.equal(false);
+                                    expect(page.displayResults()).to.equal(false);
+                                });
+                                it('Does not display any results', function () {
+                                    expect(page.results()).to.have.length(0);
+                                });
+                                // TODO Displays the error
+                            });
+                        });
+                    });
+                    afterEach(function () {
+                        container.reset();
+                    });
                 });
             });
         }
