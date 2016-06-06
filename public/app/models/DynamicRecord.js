71498eb8a2113c737e5c0928aa764df7b81b56f9
Ben New <ben.new@gaiaresources.com.au>
2016-06-06 23:34:50 +0800

    Add `DynamicRecord` model module.

---
+++ public/app/models/DynamicRecord.js
@@ -0,0 +1,43 @@
+(function () {
+    'use strict';
+
+    define(
+        [
+            'lodash',
+            'knockout',
+            'util/safelyParseJson'
+        ],
+        function (_, ko, parse) {
+            var idField = 'object_id';
+
+            return function (result, fields) {
+                result = ko.unwrap(result);
+
+                this.id = ko.pureComputed(function () {
+                    return result[idField];
+                });
+
+                this.data = ko.pureComputed(function () {
+                    return _(ko.unwrap(fields))
+                        .mapKeys('key')
+                        .mapValues(function (field) {
+                            var value = result[field.key] || '';
+                            if (field.parse) {
+                                value = parse(value);
+                                if (field.skip) {
+                                    value = _.drop(value, field.skip);
+                                }
+                                if (field.filter === true) {
+                                    value = _.filter(value);
+                                } else if (_.isString(field.filter)) {
+                                    value = _.filter(value, field.filter);
+                                }
+                            }
+                            return value;
+                        }.bind(this))
+                        .value();
+                });
+            };
+        }
+    );
+}());
