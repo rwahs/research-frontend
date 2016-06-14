2ff0c52c245601a7a13666a1271b349d7a48c80c
Ben New <ben.new@gaiaresources.com.au>
2016-06-15 02:02:06 +0800

    Add missing tests for `routes` module.

--- test/spec/config/routes.spec.js
+++ test/spec/config/routes.spec.js
@@ -20,8 +20,27 @@
                     expect(routes.detailUrlFor).to.be.a('function');
                 });
                 describe('The `searchUrlFor` function', function () {
+                    describe('With no `query` parameter', function () {
+                        it('Returns the correct URL', function () {
+                            expect(routes.searchUrlFor('type')).to.equal('/type/search');
+                            expect(routes.searchUrlFor('blizblaz')).to.equal('/blizblaz/search');
+                        });
+                    });
+                    describe('With an object `query` parameter', function () {
+                        it('Returns the correct URL', function () {
+                            expect(routes.searchUrlFor('type', { foo: 'bar', xyzzy: 'quux' })).to.equal('/type/search?foo=bar&xyzzy=quux');
+                            expect(routes.searchUrlFor('blizblaz', { a: 'b', x: 'y' })).to.equal('/blizblaz/search?a=b&x=y');
+                        });
+                    });
                 });
-                // TODO Tests for this
+                describe('The `detailUrlFor` function', function () {
+                    describe('With an `id` parameter', function () {
+                        it('Returns the correct URL', function () {
+                            expect(routes.detailUrlFor('type', 42)).to.equal('/type/detail/42');
+                            expect(routes.detailUrlFor('blizblaz', 69)).to.equal('/blizblaz/detail/69');
+                        });
+                    });
+                });
             });
         }
     );
