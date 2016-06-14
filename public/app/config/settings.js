0832a9cfda696ec0968d0aae3c35157b0616e94c
Ben New <ben.new@gaiaresources.com.au>
2016-06-14 22:58:16 +0800

    Add `labelField` to `settings` objects.
    + For library and photographs records, the label field is named `Title`,
      for museum and public memorials, it is `ItemName`.
    + Now the label can be programmatically determined based on settings.

--- public/app/config/settings.js
+++ public/app/config/settings.js
@@ -9,24 +9,28 @@
             return function () {
                 container.register('settings.library', {
                     collectionName: 'Library',
+                    labelField: 'Title',
                     searchTypes: 'config/collections/library/searchTypes',
                     searchResultFields: 'config/collections/library/searchResultFields',
                     detailFields: 'config/collections/library/detailFields'
                 });
                 container.register('settings.photographs', {
                     collectionName: 'Photographs',
+                    labelField: 'Title',
                     searchTypes: 'config/collections/photographs/searchTypes',
                     searchResultFields: 'config/collections/photographs/searchResultFields',
                     detailFields: 'config/collections/photographs/detailFields'
                 });
                 container.register('settings.museum', {
                     collectionName: 'Museum',
+                    labelField: 'ItemName',
                     searchTypes: 'config/collections/museum/searchTypes',
                     searchResultFields: 'config/collections/museum/searchResultFields',
                     detailFields: 'config/collections/museum/detailFields'
                 });
                 container.register('settings.memorials', {
                     collectionName: 'Public Memorials',
+                    labelField: 'ItemName',
                     searchTypes: 'config/collections/memorials/searchTypes',
                     searchResultFields: 'config/collections/memorials/searchResultFields',
                     detailFields: 'config/collections/memorials/detailFields'
