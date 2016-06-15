d82ec53898ce1365013713e3a389cdb6dc8432d9
Ben New <ben.new@gaiaresources.com.au>
2016-06-15 18:17:13 +0800

    Add test to list of tests to run.

--- test/main.js
+++ test/main.js
@@ -75,6 +75,7 @@
                     'spec/models/DynamicRecord.spec',
                     'spec/models/ListPager.spec',
                     'spec/models/ListSorter.spec',
+                    'spec/models/ListModeSwitcher.spec',
                     'spec/services/searchService.spec',
                     'spec/services/detailService.spec',
                     'spec/ui/components/controls/ListControlsComponent.spec',
