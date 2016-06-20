30ec630a142941a30c05868d6c916a32626b69e3
Ben New <ben.new@gaiaresources.com.au>
2016-06-20 17:42:11 +0800

    Include build date in `application.js` header.

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,7 @@
             ' * ${pkg.description}\n' +
             ' * \n' +
             ' * @version ${pkg.version}\n' +
+            ' * @date ' + (new Date()).toISOString() + '\n' +
             ' * @link ${pkg.homepage}\n' +
             ' * @license ${pkg.license}\n' +
             ' */\n\n';
